5e4c1b5808c6b11430fba03fbb43a37a9e8735dc
JLCHDEVELOP <cjl840127@163.com>
2015-04-14 13:13:52 +0800

    添加注释

--- src/com/wg/daonile/grim/utils/ConfigFile.java
+++ src/com/wg/daonile/grim/utils/ConfigFile.java
@@ -8,24 +8,31 @@ import com.wg.daonile.app.DaoNiLeApp;
 import android.content.res.AssetManager;
 
 public class ConfigFile {
-	 private static Properties prop = null;  
-	    static {   
-	        try {   
-	            prop = new Properties();
-	            AssetManager am = DaoNiLeApp.getAppInstance().getAssets();
-	            InputStream is = am.open("config.properties");
-	            prop.load(is);    
-	         
-	        }catch (Exception e) {   
-	            e.printStackTrace();
-	        }   
-	    }
-	    
-	    public static boolean isDebug(){
-	        return Boolean.valueOf(prop.get("is_debug").toString()).booleanValue();
-	    }
-	    
-	    public static boolean isCrashhandlerOpen(){
-	        return Boolean.valueOf(prop.get("is_crashhandler_open").toString()).booleanValue();
-	    }
+	private static Properties prop = null;
+	static {
+		try {
+			prop = new Properties();
+			AssetManager am = DaoNiLeApp.getAppInstance().getAssets();
+			InputStream is = am.open("config.properties");
+			prop.load(is);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
+
+	/**
+	 * 是否打印日志
+	 * @return
+	 */
+	public static boolean isDebug() {
+		return Boolean.valueOf(prop.get("is_debug").toString()).booleanValue();
+	}
+
+	/**
+	 * 是否打开全局异常收集器
+	 * @return
+	 */
+	public static boolean isCrashhandlerOpen() {
+		return Boolean.valueOf(prop.get("is_crashhandler_open").toString()).booleanValue();
+	}
+}
